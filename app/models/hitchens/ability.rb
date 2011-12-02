e7f304ad51dd82496e27d65cfbae32e97f330742
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-12-01 19:23:25 -0500

    move ability class and scope it

---
+++ app/models/hitchens/ability.rb
@@ -0,0 +1,15 @@
+module Hitchens
+  class Ability
+    include CanCan::Ability
+
+    def initialize(user)
+      user ||= User.new # guest user (not logged in)
+      if user.admin?
+        can :manage, :all
+      else
+        can :read, :all
+      end
+    end
+  end
+end
+
