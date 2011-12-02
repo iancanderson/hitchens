c3bcb4e7f9a268115fec42fce7c8ecdefaa990b7
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-12-01 20:54:07 -0500

    show publish date

--- app/decorators/hitchens/post_decorator.rb
+++ app/decorators/hitchens/post_decorator.rb
@@ -5,5 +5,9 @@ module Hitchens
     def body_to_html
       markdown(body)
     end
+
+    def pubdate_display
+      publication_date.getlocal.strftime("%B %e, %Y at %l:%M %P")
+    end
   end
 end
