0d4f5e8507fcc2fe5912fe0f7ff2295ca72beab2
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-12-01 20:24:23 -0500

    can now publish and unpublish posts

--- app/models/hitchens/post.rb
+++ app/models/hitchens/post.rb
@@ -1,5 +1,17 @@
 module Hitchens
   class Post < ActiveRecord::Base
     scope :published, lambda{ where('publication_date < ?', Time.now) }
+
+    def published=(value)
+      if ActiveRecord::ConnectionAdapters::Column.value_to_boolean(value)
+        self.publication_date = Time.now unless publication_date
+      else
+        self.publication_date = nil
+      end
+    end
+    def published
+      return false unless publication_date
+      publication_date < Time.now
+    end
   end
 end
