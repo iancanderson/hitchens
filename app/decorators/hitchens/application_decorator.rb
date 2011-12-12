8a109a6d5f3de3d4ba4d98144cae72c61c943417
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-12-11 20:52:22 -0500

    fixed duplicated pre tags with code blocks

--- app/decorators/hitchens/application_decorator.rb
+++ app/decorators/hitchens/application_decorator.rb
@@ -1,7 +1,7 @@
 module Hitchens
   class ApplicationDecorator < Draper::Base
     def markdown(text)
-      syntax_highlighter(markdown_parser.render(text)).html_safe
+      markdown_parser.render(text).html_safe
     end
 
 private
@@ -9,17 +9,6 @@ private
       @@parser ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML,
         :autolink => true, :space_after_headers => true,
         :fenced_code_blocks => true)
-    end
-
-    def syntax_highlighter(html)
-      doc = Nokogiri::HTML(html)
-      doc.search("code").each do |pre|
-        code_ray = CodeRay.scan(pre.text.rstrip, pre[:class]).div
-        # first line of code wasn't indented as much as others
-        # need to add a line break after the pre tag
-        pre.replace code_ray.sub('<pre>', "<pre>\n")
-      end
-      doc.to_s
     end
   end
 end
--- app/decorators/hitchens/post_decorator.rb
+++ app/decorators/hitchens/post_decorator.rb
@@ -3,7 +3,8 @@ module Hitchens
     decorates 'Hitchens::Post'
 
     def body_to_html
-      markdown(body)
+      html = markdown(body)
+      syntax_highlight(html).html_safe
     end
 
     def pubdate_tag
@@ -17,6 +18,26 @@ module Hitchens
   private
     def pubdate_display
       publication_date.getlocal.strftime("%A, %B %e, %Y at %l:%M %p")
+    end
+    def add_2_spaces_after_code_tags(doc)
+      # for some reason,
+      # the first line of code is indented one level fewer than other lines.
+      doc.tap do |doc|
+        doc.css("code").each do |code|
+          code.content = "  #{code.content}"
+        end
+      end
+    end
+    def syntax_highlight(html)
+      doc = Nokogiri::HTML(html)
+      doc = add_2_spaces_after_code_tags(doc)
+      # remove all pre tags.
+      # coderay will nest pre tags within the code tags.
+      doc.css("pre").each { |pre| pre.replace pre.inner_html }
+      doc.css("code").each do |code|
+        code.replace CodeRay.scan(code.text.rstrip, code[:class]).div
+      end
+      doc.to_s
     end
   end
 end
