4a4e8b6cdf6e4a8f3e646c10e2aa5b266649e9cb
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-12-11 22:17:07 -0500

    haml was indenting pre tags

--- app/decorators/hitchens/post_decorator.rb
+++ app/decorators/hitchens/post_decorator.rb
@@ -19,23 +19,14 @@ module Hitchens
     def pubdate_display
       publication_date.getlocal.strftime("%A, %B %e, %Y at %l:%M %p")
     end
-    def add_2_spaces_after_code_tags(doc)
-      # for some reason,
-      # the first line of code is indented one level fewer than other lines.
-      doc.tap do |doc|
-        doc.css("code").each do |code|
-          code.content = "  #{code.content}"
-        end
-      end
-    end
     def syntax_highlight(html)
-      doc = Nokogiri::HTML(html)
-      doc = add_2_spaces_after_code_tags(doc)
+      doc = Nokogiri::HTML.fragment(html)
       # remove all pre tags.
       # coderay will nest pre tags within the code tags.
       doc.css("pre").each { |pre| pre.replace pre.inner_html }
       doc.css("code").each do |code|
-        code.replace CodeRay.scan(code.text.rstrip, code[:class]).div
+        code_ray = CodeRay.scan(code.text.rstrip, code[:class]).div
+        code.replace code_ray
       end
       doc.to_s
     end
