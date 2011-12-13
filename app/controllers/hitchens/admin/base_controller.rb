1fb1efd1d64dc117e512459cddcb2fdaefd9fd53
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-12-12 22:13:34 -0500

    admin base controller can't find current_hitchens_user method on production

--- app/controllers/hitchens/admin/base_controller.rb
+++ app/controllers/hitchens/admin/base_controller.rb
@@ -1,19 +1,17 @@
 module Hitchens
-  module Admin
-    class BaseController < ApplicationController
-      before_filter :authenticate_hitchens_admin
-      skip_authorization_check :only => [:index]
+  class Admin::BaseController < ApplicationController
+    before_filter :authenticate_hitchens_admin
+    skip_authorization_check :only => [:index]
 
-      def index
-      end
+    def index
+    end
 
-      private
+    private
 
-      def authenticate_hitchens_admin
-        unless current_hitchens_user && current_hitchens_user.blog_admin?
-          flash.alert = t("hitchens.errors.access_denied")
-          redirect_to main_app.__send__(Hitchens.sign_in_url_helper)
-        end
+    def authenticate_hitchens_admin
+      unless current_hitchens_user && current_hitchens_user.blog_admin?
+        flash.alert = t("hitchens.errors.access_denied")
+        redirect_to main_app.__send__(Hitchens.sign_in_url_helper)
       end
     end
   end
