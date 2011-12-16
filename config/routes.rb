dc6ebe0210776dccadb4b58b210b2488e9169b73
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-12-15 22:13:33 -0500

    oops. new posts path was masking /admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Hitchens::Engine.routes.draw do
 
-  get '/:id' => 'posts#show', :as => :post
-
   root :to => 'posts#index'
 
   namespace :admin do
     root :to => "base#index"
     resources :posts
   end
+
+  get '/:id' => 'posts#show', :as => :post
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => redirect("/blog")
+  match 'admin' => redirect('/blog/admin')
   mount Hitchens::Engine => "/blog"
 end
